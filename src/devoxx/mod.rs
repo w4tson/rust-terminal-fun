3edfa898ad0c665fb3cf8032c4a2bef9a01b6872
Paul Watson <paulwatson@Pauls-MacBook-Pro.local>
2019-10-17 17:05:04 +0100

    foo

--- src/devoxx/mod.rs
+++ src/devoxx/mod.rs
@@ -1,7 +1,8 @@
 pub mod model;
 use std::fs;
 use crate::devoxx::model::{ScheduleItem};
-use chrono::Weekday;
+use chrono::{Weekday, DateTime, Utc};
+use std::str::FromStr;
 
 pub fn get_talks_by_weekday(day: &Weekday) -> Result<Vec<ScheduleItem>, failure::Error> {
     let day = match day {
@@ -22,6 +23,34 @@ pub fn get_talks_by_day(day: &str) -> Result<Vec<ScheduleItem>, failure::Error> 
         .map_err(failure::Error::from)
 }
 
+#[derive(Debug)]
+struct ScheduleItem2 {
+    talk_title: String,
+    from_date: DateTime<Utc>
+}
+
+pub fn foo() {
+    let content  = fs::read_to_string("./devoxx-data/talks.txt").unwrap();
+    let lines = content.lines().map(to_schedule).collect::<Vec<ScheduleItem2>>();
+    eprintln!("lines = {:#?}", lines);
+    for line in lines {
+
+    }
+
+}
+
+fn to_schedule(line :&str) -> ScheduleItem2 {
+    let foo = line.split(",").collect::<Vec<&str>>();
+    let talk_title = foo.get(0).unwrap().to_string();
+    let from_date = foo.get(1).unwrap();
+//    eprintln!("title = {}", talk_title);
+//    eprintln!("date = {}", from_date);
+    let from_date = DateTime::from_str(from_date).expect("should be a date");
+    ScheduleItem2 { talk_title, from_date }
+
+//    eprintln!("scedule = {:#?}", schedule);
+}
+
 
 
 #[cfg(test)]
@@ -35,5 +64,10 @@ mod tests {
         if let Ok(talks) = talks {
             assert_eq!(talks.len(), 38);
         }
+    }
+
+    #[test]
+    fn test2() {
+        foo();
     }
 }
